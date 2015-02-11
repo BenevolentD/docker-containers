8e96eec6c1428e9b5f19227c31d357dcc79f05bc
BenevolentD <michael.katona@gmail.com>
2015-02-10 19:25:39 -0800

    firstrun

--- mumble/firstrun.sh
+++ mumble/firstrun.sh
@@ -3,8 +3,10 @@
 # Check if config exists. If not, copy in the sample config
 if [ -f /config/mumble-server.ini ]; then
   echo "Using existing config file."
+  rm /etc/mumble-server.ini
+  ln -s /config/mumble-server.ini /etc/mumble-server.ini
 else
-  echo "Creating config from template."
-  mv /etc/mumble-server.ini /config/mumble-server.ini
+  echo "Using default config file."
+  ln -s /etc/mumble-server.ini /config/mumble-server.ini
   chown nobody:users /config/mumble-server.ini
 fi
