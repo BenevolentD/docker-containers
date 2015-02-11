f047b21a2376ae32479417e7376da1af3168e5c1
BenevolentD <michael.katona@gmail.com>
2015-02-10 18:37:42 -0800

    bleh

--- mumble/firstrun.sh
+++ mumble/firstrun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check if config exists. If not, copy in the sample config
-if [ -f /config/ddclient.conf ]; then
+if [ -f /config/mumble-server.ini ]; then
   echo "Using existing config file."
 else
   echo "Creating config from template."
-  mv /etc/ddclient.conf-sample /config/ddclient.conf
-  chown nobody:users /config/ddclient.conf
+  mv /etc/mumble-server.ini /config/mumble-server.ini
+  chown nobody:users /config/mumble-server.ini
 fi
