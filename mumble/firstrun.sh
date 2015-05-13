47f08df60f8fc38dda6c89efa2c9c1f9893db95a
BenevolentD <michael.katona@gmail.com>
2015-05-13 11:59:24 -0700

    change script

--- mumble/firstrun.sh
+++ mumble/firstrun.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Check if config exists. If not, copy in the sample config
-if [ -f /config/mumble-server.ini ]; then
-  echo "Using existing config file."
-  rm /etc/mumble-server.ini
-  ln -s /config/mumble-server.ini /etc/mumble-server.ini
-else
-  echo "Using default config file."
-  mv /etc/mumble-server.ini /config/mumble-server.ini
-  ln -s /config/mumble-server.ini /etc/mumble-server.ini
-  chown nobody:users /config/mumble-server.ini
+if [ ! -f /config/mumble-server.ini ]
+then
+  sed -i 's/var.log.mumble-server/data/' /etc/mumble-server.ini
+  sed -i 's/var.lib.mumble-server/data/' /etc/mumble-server.ini
+  sed -i 's/^INIFILE=.*/INIFILE=\/config\/mumble-server.ini/' /etc/init.d/mumble-server
+  cp /etc/mumble-server.ini /config
+  chmod a+rw /config/mumble-server.ini
+  echo Created /config/mumble-server.ini. Exiting.
+  exit 1
 fi
+
+echo Starting mumble-server service
+sed -i 's/^INIFILE=.*/INIFILE=\/config\/mumble-server.ini/' /etc/init.d/mumble-server
+service mumble-server start
+
+while true
+do
+  sleep 3600
+done
