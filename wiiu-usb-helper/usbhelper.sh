dddcb326106c8314a2606c21bf0eb51c0c153370
BenevolentD <BenevolentD@users.noreply.github.com>
2018-03-20 12:13:40 -0700

    aa

--- wiiu-usb-helper/usbhelper.sh
+++ wiiu-usb-helper/usbhelper.sh
@@ -2,6 +2,7 @@
 
 if [ ! -e "/root/usbhelper_installed" ] ; then
     cd /root/wiiuusbhelper
+    sh winetricks -q msxml3 dotnet45 vcrun2010 win7 && \
     wget https://application.wiiuusbhelper.com/Updater.exe
     touch "/root/usbhelper_installed"
 fi
